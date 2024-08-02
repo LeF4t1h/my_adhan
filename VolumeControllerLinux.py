c7205ab6a6158c20c128318fecc1b5f7cd55c585
Fatih Balci <98948896+LeF4t1h@users.noreply.github.com>
2024-08-02 17:13:43 +0200

    added missing method

--- VolumeControllerLinux.py
+++ VolumeControllerLinux.py
@@ -7,6 +7,10 @@ class VolumeController:
         self.sink = self.get_default_sink()
         self.muted = self.sink.mute
 
+    def get_default_sink(self):
+        # Get the default sink (output device)
+        return self.pulse.get_sink_by_name(self.pulse.server_info().default_sink_name)
+
     def toggle_mute(self):
         # Toggle mute state
         self.muted = not self.muted
