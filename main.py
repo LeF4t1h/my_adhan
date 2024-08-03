f8e525673f5636ad18c4a794d78c6de1fbccf5b9
Fatih Balci <98948896+LeF4t1h@users.noreply.github.com>
2024-08-03 20:48:44 +0200

    removed an import

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from datetime import datetime, timedelta
-from VolumeControllerWindows import *
 import vlc
 import re
 import platform
